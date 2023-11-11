52702e36e24df4a168df66e5eb2ad05ebd0ef0bb
lazuskica <lazuskica@gmail.com>
2023-11-11 11:33:35 +0100

    test
    uspeo

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -4,14 +4,9 @@
  * @file           : main.c
  * @brief          : Main program body
  ******************************************************************************
- * @attention
  *
- * Copyright (c) 2023 STMicroelectronics.
- * All rights reserved.
- *
- * This software is licensed under terms that can be found in the LICENSE file
- * in the root directory of this software component.
- * If no LICENSE file comes with this software, it is provided AS-IS.
+ *	  Created on: Oct 19, 2023
+ *	      Author: lazar
  *
  ******************************************************************************
  */
